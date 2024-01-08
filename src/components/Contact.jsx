506ff6757eb8d38b427545f8136ce1efe260894b
wealthymanyasa <wealthymanyasa@gmail.com>
2024-01-08 12:14:18 +0200

    added new projects and list

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -56,6 +56,36 @@ const Contact = () => {
       ))
      }, (error) => {
          // show the user an error
+         
+      toast.custom((t) => (
+        <div
+          className={`${
+            t.visible ? 'animate-enter' : 'animate-leave'
+          } max-w-md w-full bg-red-100 border-2 border-red-300 shadow-red-900 animate-bounce shadow-lg rounded-lg pointer-events-auto flex ring-1 ring-black ring-opacity-5`}
+        >
+          <div className="flex-1 w-0 p-4">
+            <div className="flex items-start">
+              <div className="flex-shrink-0 pt-0.5">
+                <img
+                  className="h-10 w-10 rounded-full"
+                  src={profile}
+                  alt="profile"
+                />
+              </div>
+              <div className=" flex-1">
+                <p className="text-sm font-medium text-gray-900">
+                Message sending failed.
+                </p>
+                <p className="mt-1 text-sm text-gray-500">
+                  Please try again later!!
+                </p>
+              </div>
+            
+            </div>
+          </div>
+         
+        </div>
+      ))
        console.log(error.text);
 
      });
